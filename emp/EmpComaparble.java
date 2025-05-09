f7d1ad68f821440af4dee9e12ef389ee84efe3fb
Akhila-github-dev <akhilahere3@gmail.com>
2025-05-09 10:54:51 +0530

    Update EmpComaparble.java

--- emp/EmpComaparble.java
+++ emp/EmpComaparble.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class EmpComaparble implements Comparable<Employee> {
+public class EmpComaparble implements Comparable<EmpComaparble> {
 
 		String name;
 		int age;
@@ -20,7 +20,7 @@ public class EmpComaparble implements Comparable<Employee> {
 		}
 
 		@Override
-		public int compareTo(Employee e) {
+		public int compareTo(EmpComaparble e) {
 
 			int n = this.age - e.age;
 			if (n == 0) {
@@ -36,16 +36,16 @@ public class EmpComaparble implements Comparable<Employee> {
 
 		public static void main(String[] args) {
 		
-	        List<Employee> list = new ArrayList<>();
-			list.add(new Employee("akhi", 19));
-			list.add(new Employee("bhanu", 20));
-			list.add(new Employee("akhi", 20));
-			list.add(new Employee("prassu", 22));
-			list.add(new Employee("akhi", 1));
-			list.add(new Employee("bhanu", 19));
-			list.add(new Employee("bhanu", 28));
+	        List<EmpComaparble> list = new ArrayList<>();
+			list.add(new EmpComaparble("akhi", 19));
+			list.add(new EmpComaparble("bhanu", 20));
+			list.add(new EmpComaparble("akhi", 20));
+			list.add(new EmpComaparble("prassu", 22));
+			list.add(new EmpComaparble("akhi", 1));
+			list.add(new EmpComaparble("bhanu", 19));
+			list.add(new EmpComaparble("bhanu", 28));
 
-			Collections.sort(list, new Employee());
+			Collections.sort(list);
 			System.out.println(list);
 		}
 
