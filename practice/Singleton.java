5e2377da189e80145080a4a7e8c3e6e59486a58d
Akhila-github-dev <akhilahere3@gmail.com>
2025-07-14 06:45:30 +0530

    Update Singleton.java

--- practice/Singleton.java
+++ practice/Singleton.java
@@ -2,13 +2,13 @@
 
 public class Singleton {
 
-	private static Singleton sing;
+	private static Singleton sing; //This is the only instance of the class that will exist.
 	
-    private Singleton() {
+    private Singleton() {// private constructor prevents external instantiation
     	
   }
-	
-    public static Singleton getObjeSingleton() {
+	//Provide a public static method to get the instance
+    public static Singleton getObjeSingleton() {//This method returns the instance, creating it only if it doesn't already exist.
     	
     	if(sing==null) {
     		synchronized (Singleton.class) {
